5989bb4d9b3e819a16707831a235b52ee4dd45fc
Andrew Serplus <andrew.serplus@hotmail.com>
2018-03-17 22:38:06 -0700

    chore) Enabled ES Lint

--- tests/dataStructures/queue/index.test.js
+++ tests/dataStructures/queue/index.test.js
@@ -1,133 +1,133 @@
 const should = require('should');
-const Queue = require('../../../dataStructures/queue/index');
+const Queue = require('../../../app/dataStructures/queue/index');
 
 describe('dataStructure/queue', () => {
-    let objectUnderTest;
-    const provided = [1, 2, 3];
+  let objectUnderTest;
+  const provided = [1, 2, 3];
 
-    beforeEach(() => {
-        objectUnderTest = new Queue();
+  beforeEach(() => {
+    objectUnderTest = new Queue();
+  });
+
+  describe('enqueue()', () => {
+    it('should add items to end of queue', () => {
+      /* arrange */
+      const expected = [];
+
+      provided.forEach((item) => {
+        /* act */
+        objectUnderTest.enqueue(item);
+
+        expected.push(item);
+
+        /* assert */
+        objectUnderTest.items.should.deepEqual(expected);
+      });
+    });
+  });
+
+  describe('dequeue()', () => {
+    it('should throw "Underflow" error', () => {
+      should(() => objectUnderTest.dequeue()).throw('Underflow');
     });
 
-    describe('enqueue()', () => {
-        it('should add items to end of queue', () => {
-            /* arrange */
-            const expected = [];
+    it('should remove first item from queue', () => {
+      /* arrange */
+      provided.forEach(item => objectUnderTest.enqueue(item));
+      const expected = [provided[1], provided[2]];
 
-            provided.forEach(item => {
-                /* act */
-                objectUnderTest.enqueue(item);
+      /* act */
+      objectUnderTest.dequeue();
 
-                expected.push(item);
-
-                /* assert */
-                objectUnderTest.items.should.deepEqual(expected);
-            });
-        });
+      /* assert */
+      objectUnderTest.items.should.deepEqual(expected);
     });
 
-    describe('dequeue()', () => {
-        it('should throw "Underflow" error', () => {
-            should(() => objectUnderTest.dequeue()).throw('Underflow')
-        });
+    it('should return first item from queue', () => {
+      /* arrange */
+      provided.forEach(item => objectUnderTest.enqueue(item));
+      const expected = provided[0];
 
-        it('should remove first item from queue', () => {
-            /* arrange */
-            provided.forEach(item => objectUnderTest.enqueue(item));
-            const expected = [provided[1], provided[2]];
+      /* act */
+      const result = objectUnderTest.dequeue();
 
-            /* act */
-            const result = objectUnderTest.dequeue();
+      /* assert */
+      result.should.equal(expected);
+    });
+  });
 
-            /* assert */
-            objectUnderTest.items.should.deepEqual(expected);
-        });
+  describe('front()', () => {
+    it('should return undefined if queue is empty', () => {
+      /* arrange */
+      const expected = undefined;
 
-        it('should return first item from queue', () => {
-            /* arrange */
-            provided.forEach(item => objectUnderTest.enqueue(item));
-            const expected = provided[0];
+      /* act */
+      const result = objectUnderTest.front();
 
-            /* act */
-            const result = objectUnderTest.dequeue();
-
-            /* assert */
-            result.should.equal(expected);
-        });
+      /* assert */
+      should.equal(result, expected);
     });
 
-    describe('front()', () => {
-        it('should return undefined if queue is empty', () => {
-            /* arrange */
-            const expected = undefined;
+    it('should return first item from queue', () => {
+      /* arrange */
+      provided.forEach(item => objectUnderTest.enqueue(item));
+      const expected = provided[0];
 
-            /* act */
-            const result = objectUnderTest.front();
+      /* act */
+      const result = objectUnderTest.front();
 
-            /* assert */
-            should.equal(result, expected);
-        });
+      /* assert */
+      result.should.equal(expected);
+    });
+  });
 
-        it('should return first item from queue', () => {
-            /* arrange */
-            provided.forEach(item => objectUnderTest.enqueue(item));
-            const expected = provided[0];
+  describe('rear()', () => {
+    it('should return undefined if queue is empty', () => {
+      /* arrange */
+      const expected = undefined;
 
-            /* act */
-            const result = objectUnderTest.front();
+      /* act */
+      const result = objectUnderTest.rear();
 
-            /* assert */
-            result.should.equal(expected);
-        });
+      /* assert */
+      should.equal(result, expected);
     });
 
-    describe('rear()', () => {
-        it('should return undefined if queue is empty', () => {
-            /* arrange */
-            const expected = undefined;
+    it('should return last item from queue', () => {
+      /* arrange */
+      provided.forEach(item => objectUnderTest.enqueue(item));
+      const expected = provided[2];
 
-            /* act */
-            const result = objectUnderTest.rear();
+      /* act */
+      const result = objectUnderTest.rear();
 
-            /* assert */
-            should.equal(result, expected);
-        });
+      /* assert */
+      result.should.equal(expected);
+    });
+  });
 
-        it('should return last item from queue', () => {
-            /* arrange */
-            provided.forEach(item => objectUnderTest.enqueue(item));
-            const expected = provided[2];
+  describe('isEmpty()', () => {
+    it('should return true', () => {
+      /* arrange */
+      const expected = true;
 
-            /* act */
-            const result = objectUnderTest.rear();
+      /* act */
+      const result = objectUnderTest.isEmpty();
 
-            /* assert */
-            result.should.equal(expected);
-        });
+      /* assert */
+      result.should.equal(expected);
     });
 
-    describe('isEmpty()', () => {
-        it('should return true', () => {
-            /* arrange */
-            const expected = true;
+    it('should return false', () => {
+      /* arrange */
+      provided.forEach(item => objectUnderTest.enqueue(item));
+      const expected = false;
 
-            /* act */
-            const result = objectUnderTest.isEmpty();
+      /* act */
+      const result = objectUnderTest.isEmpty();
 
-            /* assert */
-            result.should.equal(expected);
-        });
-
-        it('should return false', () => {
-            /* arrange */
-            provided.forEach(item => objectUnderTest.enqueue(item));
-            const expected = false;
-
-            /* act */
-            const result = objectUnderTest.isEmpty();
-
-            /* assert */
-            result.should.equal(expected);
-        });
+      /* assert */
+      result.should.equal(expected);
     });
+  });
 });
